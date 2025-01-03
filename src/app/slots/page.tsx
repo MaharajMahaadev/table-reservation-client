5993798a32702696cae4bc6342ad118517708b72
MaharajMahaadev <maharajmahaadev2@gmail.com>
2025-01-03 22:02:55 +0530

    Minor Changes

--- src/app/slots/page.tsx
+++
@@ -1,5 +0,0 @@
-import DateView from "@/ui/DateView.jsx";
-
-export default function Page() {
-  return <DateView />;
-}
--- src/ui/Bookings.tsx
+++ src/ui/Bookings.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import DateView from "@/ui/DateView.jsx";
+import DateView from "@/app/slots/page.jsx";
 import Booked from "@/ui/Booked.jsx";
 
 export default function Bookings() {
